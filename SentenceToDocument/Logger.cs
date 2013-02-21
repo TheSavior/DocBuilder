2e4f927f64f84ed0671996eada27d75a8982427f
TheSavior <github@eli-white.com>
2013-02-20 16:12:19 -0800

    Updated to run in a single pass.

---
+++ SentenceToDocument/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentenceToDocument
+{
+    public class Logger
+    {
+        private const string LOG = @"C:\Users\Eli\Desktop\output\log.txt";
+
+        public static void WriteLine(string text)
+        {
+            Write(text + "\n");
+        }
+
+        public static void Write(string text)
+        {
+            using (StreamWriter outfile = new StreamWriter(LOG, true))
+            {
+                outfile.WriteLine(text);
+                Console.Write(text);
+            }
+        }
+    }
+}
