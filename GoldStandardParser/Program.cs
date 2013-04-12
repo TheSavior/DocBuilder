aebf2e8d2eb8915cd154f9a722612c4fe9a8cb72
TheSavior <github@eli-white.com>
2013-04-11 19:21:29 -0700

    Only entities that aren't null

--- GoldStandardParser/Program.cs
+++ GoldStandardParser/Program.cs
@@ -13,6 +13,8 @@ namespace GoldStandardParser
     {
         private const string GOLD_XML = @"C:\Users\Eli\Documents\tac_2010_kbp_evaluation_entity_linking_queries.xml";
         private const string GOLD_TAB = @"C:\Users\Eli\Documents\tac_2010_kbp_evaluation_entity_linking_query_types.tab";
+        private const string ENTITY_LOOKUP = @"C:\Users\Eli\Documents\GitHub\NEL\entityLookup.txt";
+        private const string ENTITY_LOOKUP_NEW = @"C:\Users\Eli\Documents\GitHub\NEL\entityLookupNew.txt";
         private const string OUTPUT = @"C:\Users\Eli\Documents\doc_gold.txt";
 
         static void Main(string[] args)
@@ -24,7 +26,8 @@ namespace GoldStandardParser
             var tabs = 
                 (from line in File.ReadLines(GOLD_TAB)
                 let pieces = line.Split('\t')
-                select new {QueryId = pieces[0], EntityId = pieces[1]})
+                let entityId = pieces[1]
+                select new {QueryId = pieces[0], EntityId = entityId})
                 .ToDictionary(item => item.QueryId, item => item.EntityId);
 
             using (XmlReader reader = XmlReader.Create(new StreamReader(GOLD_XML)))
@@ -55,26 +58,45 @@ namespace GoldStandardParser
                     from el in root.Elements("query")
                     let docid = (string)el.Element("docid")
                     let entityId = tabs[((string)el.Attribute("id"))]
-                    where !docid.StartsWith("eng-")
+                    where !docid.StartsWith("eng-") && !entityId.StartsWith("NIL")
                     group entityId by docid into ents
                     orderby ents.Key
                     select new { Document = ents.Key, Entities = ents};
 
                 docs = items.Count();
 
+                HashSet<String> lookup = new HashSet<string>();
+
                 using (StreamWriter outfile = new StreamWriter(OUTPUT))
                 {
                     foreach (var item in items)
                     {
+                        foreach(var str in item.Entities) {
+                                lookup.Add(str);
+                        }
                         entities += item.Entities.Count();
 
                         outfile.Write(string.Format("{0}\t{1}\n", item.Document, String.Join("\t", item.Entities)));
                     }
                 }
+
+                LimitLookup(lookup);
             }
 
             Console.Write(string.Format("Done. {0} documents and {1} entities", docs, entities));
             Console.ReadKey();
+        }
+
+        public static void LimitLookup(HashSet<String> ents) {
+            var tabs =
+                (from line in File.ReadLines(ENTITY_LOOKUP)
+                 let pieces = line.Split('\t')
+                 let entityId = pieces[0]
+                 where ents.Contains(entityId)
+                 orderby entityId
+                 select line);
+
+            File.WriteAllLines(ENTITY_LOOKUP_NEW, tabs);
         }
     }
 }
